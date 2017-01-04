389250bd3f7a2b747716ee8d801f76e2b0a89ccd
Anton <Lazy.ant.mail@gmail.com>
2017-01-04 16:23:59 +0300

    Add sort function for transactions

--- src/containers/VisibleTransactionList.js
+++ src/containers/VisibleTransactionList.js
@@ -1,8 +1,14 @@
 import { connect } from 'react-redux';
 import TransactionList from '../components/TransactionList';
 
+const sortTransactions = (first, second) => {
+  var firstDate = new Date(first.date);
+  var secondDate = new Date(second.date);
+  return secondDate - firstDate;
+};
+
 const mapStateToProps = (state) => ({
-  transactions: state.transactions
+  transactions: state.transactions.sort(sortTransactions)
 });
 
 const VisibleTransactionList = connect(
