ddc0fc6f955d028923b5701f56db290785ba9e0f
Anton <Lazy.ant.mail@gmail.com>
2017-01-14 16:44:53 +0300

    Add some styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
 import React, { PropTypes }  from 'react';
 import { Link } from 'react-router';
-import '../styles/menu.css';
+import '../styles/app.css';
+import '../styles/header.css';
 
 const App = (props) => (
   <div className='container'>
-    <h1>App</h1>
-    <ul className="menu">
-      <li className="menu__item">
-        <Link to='/' className="menu__link" activeClassName='menu__link_active' onlyActiveOnIndex>Home</Link>
-      </li>
-      <li className="menu__item">
-        <Link to='/categories' className="menu__link" activeClassName='menu__link_active'>Categories</Link>
-      </li>
-      <li className="menu__item">
-        <Link to='/statistic' className="menu__link" activeClassName='menu__link_active'>Statistic</Link>
-      </li>
-      <li className="menu__item">
-        <Link to='/about' className="menu__link" activeClassName='menu__link_active'>About</Link>
-      </li>
-    </ul>
+    <header className='header'>
+      <h1>App</h1>
+      <ul className="menu">
+        <li className="menu__item">
+          <Link to='/' className="menu__link" activeClassName='menu__link_active' onlyActiveOnIndex>Home</Link>
+        </li>
+        <li className="menu__item">
+          <Link to='/categories' className="menu__link" activeClassName='menu__link_active'>Categories</Link>
+        </li>
+        <li className="menu__item">
+          <Link to='/statistic' className="menu__link" activeClassName='menu__link_active'>Statistic</Link>
+        </li>
+        <li className="menu__item">
+          <Link to='/about' className="menu__link" activeClassName='menu__link_active'>About</Link>
+        </li>
+      </ul>
+    </header>
     {props.children}
   </div>
 );
