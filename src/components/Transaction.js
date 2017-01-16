c5f5e951fbfc2b47ee9f7c6dd2964b98825c7f53
Anton <Lazy.ant.mail@gmail.com>
2017-01-16 19:52:52 +0300

    Change button to icon

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { TableRow, TableRowColumn } from 'material-ui/Table';
-import RaisedButton from 'material-ui/RaisedButton';
+import IconDelete from 'material-ui/svg-icons/action/delete';
 import '../styles/transaction.css';
 
 const Transaction = props => {
@@ -9,6 +9,10 @@ const Transaction = props => {
   month = month < 10 ? "0" + month : month;
   let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
   const dateString = day + "." + month + "." + date.getFullYear();
+
+  const iconStyle = {
+    cursor: "pointer",
+  };
 
   return (
     <TableRow className="transaction" hoverable selectable={false}>
@@ -19,7 +23,12 @@ const Transaction = props => {
       </TableRowColumn>
       <TableRowColumn>
         <div className="transaction__toolbar">
-          <RaisedButton  onClick={props.onDelete}>DELETE</RaisedButton >
+          <IconDelete
+            onClick={props.onDelete}
+            style={iconStyle}
+            color={"rgba(0, 0, 0, 0.541176)"}
+            hoverColor={"rgba(0, 0, 0, 0.7)"}
+          />
         </div>
       </TableRowColumn>
     </TableRow>
