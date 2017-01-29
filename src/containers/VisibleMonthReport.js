d9ff779631a89109356b91e2408d5cc303f03d53
Anton <Lazy.ant.mail@gmail.com>
2017-01-29 17:09:22 +0300

    refactoring

--- src/containers/VisibleMonthReport.js
+++ src/containers/VisibleMonthReport.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
 import RangeReport from '../components/RangeReport';
 
+const isSelectedMonth = (transaction, month) => transaction.date.getMonth() === month;
+const isIncome = transaction => transaction.type === 'income';
+
 const getReportByMonth = (transactions, month) => {
   const transactionsCount = transactions.length;
   let income = 0;
   let expenses = 0;
   for (let i = 0; i < transactionsCount; i++) {
     const transaction = transactions[i];
-    if (transaction.date.getMonth() === month) {
-      if (transaction.type === 'income') {
+    if (isSelectedMonth(transaction, month)) {
+      if (isIncome(transaction)) {
         income += transaction.amount;
       } else {
         expenses += transaction.amount;
