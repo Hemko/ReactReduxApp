ac407d70c922a2c21357ab593c6fe393c4a7bec6
Anton <Lazy.ant.mail@gmail.com>
2017-01-31 19:41:59 +0300

    fix about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import '../styles/about.css';
 
 const About = () => (
-  <div>
-    <h1>This is about page!</h1>
-    <ul className="technology-list">
-      <li className="technology-list__item">React</li>
-      <li className="technology-list__item">Redux</li>
-      <li className="technology-list__item">React Router</li>
-      <li className="technology-list__item">Redux Form</li>
-      <li className="technology-list__item">Redux Saga</li>
-      <li className="technology-list__item">Material UI</li>
-    </ul>
-  </div>
+  <MuiThemeProvider>
+    <div className="row">
+      <div className="small-12 columns">
+        <h1>This is about page!</h1>
+        <ul className="technology-list">
+          <li className="technology-list__item">React</li>
+          <li className="technology-list__item">Redux</li>
+          <li className="technology-list__item">React Router</li>
+          <li className="technology-list__item">Redux Form</li>
+          <li className="technology-list__item">Redux Saga</li>
+          <li className="technology-list__item">Material UI</li>
+        </ul>
+      </div>
+    </div>
+  </MuiThemeProvider>
 );
 
 export default About;
