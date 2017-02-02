e440f7bdbb4a97daa6415ab708bee0fdc5a15f9b
Anton <Lazy.ant.mail@gmail.com>
2017-02-02 19:18:21 +0300

    Fix header layout

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,24 @@ import '../styles/header.css';
 const App = (props) => (
   <div>
     <header className='header'>
-      <Link to='/' className="header__logo"><h1>App</h1></Link>
-      <ul className="menu">
-        <li className="menu__item">
-          <Link to='/categories' className="menu__link" activeClassName='menu__link_active'>Categories</Link>
-        </li>
-        <li className="menu__item">
-          <Link to='/statistic' className="menu__link" activeClassName='menu__link_active'>Statistic</Link>
-        </li>
-        <li className="menu__item">
-          <Link to='/about' className="menu__link" activeClassName='menu__link_active'>About</Link>
-        </li>
-      </ul>
+      <div className="row align-justify align-middle">
+        <div className="columns">
+          <Link to='/' className="header__logo"><h1>App</h1></Link>
+        </div>
+        <div className="columns">
+          <ul className="menu">
+            <li className="menu__item">
+              <Link to='/categories' className="menu__link" activeClassName='menu__link_active'>Categories</Link>
+            </li>
+            <li className="menu__item">
+              <Link to='/statistic' className="menu__link" activeClassName='menu__link_active'>Statistic</Link>
+            </li>
+            <li className="menu__item">
+              <Link to='/about' className="menu__link" activeClassName='menu__link_active'>About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
     <div className='container'>
       <MuiThemeProvider>
