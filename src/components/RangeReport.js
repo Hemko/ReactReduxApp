2a4b8e54d593ea4de3d5ef5f738f13227f085133
Anton <Lazy.ant.mail@gmail.com>
2017-02-03 19:46:14 +0300

    Add "total" for the range report

--- src/components/RangeReport.js
+++ src/components/RangeReport.js
@@ -6,6 +6,7 @@ const RangeReport = (props) => (
     <p className="range-report__header">{props.label}</p>
     <p className="range-report__item range-report__item_income">Income: {props.income}</p>
     <p className="range-report__item range-report__item_expenses">Expenses: {props.expenses}</p>
+    <p className="range-report__item">Total: {props.income - props.expenses}</p>
   </div>
 );
 
