bb63eca5b653daf44a6767857f7da51a1fab8a87
Anton <Lazy.ant.mail@gmail.com>
2017-02-07 19:31:41 +0300

    Add helmet to the about page

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,7 @@ const About = () => (
         <li className="technology-list__item">React Router</li>
         <li className="technology-list__item">Redux Form</li>
         <li className="technology-list__item">Redux Saga</li>
+        <li className="technology-list__item">React Helmet</li>
         <li className="technology-list__item">Material UI</li>
         <li className="technology-list__item">d3.js</li>
         <li className="technology-list__item">Immutable.js</li>
