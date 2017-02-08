eb270158909c43a3a766a2b93e4c8eaf371367bb
Anton <Lazy.ant.mail@gmail.com>
2017-02-08 17:49:16 +0300

    Init D3LineChart

---
+++ src/components/D3LineChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import d3 from 'd3';
+
+class D3LineChart extends React.Component {
+
+  render () {
+    return <svg></svg>;
+  }
+}
+
+export default D3LineChart;
--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import VisibleMonthReport from '../containers/VisibleMonthReport';
+import D3LineChart from './D3LineChart';
 import '../styles/statistic.css';
 
 const Statistic = () => (
   <div className="row">
     <div className="medium-12 large-4 columns">
       <VisibleMonthReport />
+    </div>
+    <div className="medium-12 large-8 columns">
+      <D3LineChart />
     </div>
   </div>
 );
