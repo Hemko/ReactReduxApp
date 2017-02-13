7149c2f2e2e01dba85a19009281edb11fb6bb392
Anton <Lazy.ant.mail@gmail.com>
2017-02-13 19:05:52 +0300

    Refactoring

--- src/components/D3LineChart.js
+++ src/components/D3LineChart.js
@@ -7,32 +7,32 @@ class D3LineChart extends React.Component {
     const style = {
       border: "1px solid black"
     };
-    
+
     const data = this.props.data;
 
     const margin = {top: 20, right: 20, bottom: 30, left: 50},
       w = this.props.width - (margin.left + margin.right),
       h = this.props.height - (margin.top + margin.bottom);
 
-    var parseDate = d3.timeParse("%m-%d-%Y");
+    const parseDate = d3.timeParse("%m-%d-%Y");
 
     data.forEach(function (d) {
       d.date = parseDate(d.day);
     });
 
-    var x = d3.scaleTime()
+    const x = d3.scaleTime()
       .domain(d3.extent(data, function(d) {
         return d.date;
       }))
       .rangeRound([0, w]);
 
-    var y = d3.scaleLinear()
+    const y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) {
         return d.count;
       })])
       .range([h, 0]);
 
-    var line = d3.line()
+    const line = d3.line()
       .x(function (d) {
         return x(d.date);
       })
@@ -41,7 +41,7 @@ class D3LineChart extends React.Component {
       })
       .curve(d3.curveCardinal);
 
-    var transform = "translate(" + margin.left + "," + margin.top + ")";
+    const transform = "translate(" + margin.left + "," + margin.top + ")";
 
     return <svg ref="svg" width={this.props.width} height={this.props.height} style={style}>
       <g transform={transform}>
