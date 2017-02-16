cde655379f356282610bc315bef0bdefd8b442ba
Anton <Lazy.ant.mail@gmail.com>
2017-02-16 18:17:18 +0300

    Display ID at page with information about transaction

--- src/components/SingleTransaction.js
+++ src/components/SingleTransaction.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const SingleTransaction = () => (
+const SingleTransaction = (props) => (
   <div className="row">
     <div className="columns">
       <h1>This is page with information about transaction!</h1>
+      ID: {props.params.id}
     </div>
   </div>
 );
