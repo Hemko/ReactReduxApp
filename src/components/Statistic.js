2fff78a52a52ea284036eb8f771a778595485d45
Anton <Lazy.ant.mail@gmail.com>
2017-02-19 17:50:15 +0300

    remove data from statistic

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -3,24 +3,13 @@ import VisibleMonthReport from '../containers/VisibleMonthReport';
 import VisibleD3LineChart from '../containers/VisibleD3LineChart';
 import '../styles/statistic.css';
 
-var data=[
-  {day:'02-11-2016',count:180},
-  {day:'02-12-2016',count:250},
-  {day:'02-13-2016',count:150},
-  {day:'02-14-2016',count:496},
-  {day:'02-15-2016',count:140},
-  {day:'02-16-2016',count:380},
-  {day:'02-17-2016',count:100},
-  {day:'02-18-2016',count:150}
-];
-
 const Statistic = () => (
   <div className="row">
     <div className="medium-12 large-4 columns">
       <VisibleMonthReport />
     </div>
     <div className="medium-12 large-8 columns">
-      <VisibleD3LineChart width="400" height="300" data={data}/>
+      <VisibleD3LineChart width="400" height="300" />
     </div>
   </div>
 );
