795b0c72eaf77f6612701fa158c807ddcd442aa9
Anton <Lazy.ant.mail@gmail.com>
2017-02-24 09:48:25 +0300

    Add additional information

--- src/components/SingleTransaction.js
+++ src/components/SingleTransaction.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SingleTransaction = (props) => (
   <div className="row">
     <div className="columns">
-      Transaction ID: {props.params.id}
+      <div>Transaction ID: {props.transaction.id}</div>
+      <div>{props.transaction.amount}</div>
     </div>
   </div>
 );
 
-export default SingleTransaction;
+const mapStateToProps = (state, props) => ({
+  transaction: state.transactions
+    .filter(transaction => parseInt(transaction.id, 10) === parseInt(props.params.id, 10))[0]
+});
+
+export default connect(mapStateToProps)(SingleTransaction);
