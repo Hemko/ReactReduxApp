70e1cb3e0bd79360e39f44509f80f9dcd92130e5
Anton <Lazy.ant.mail@gmail.com>
2017-02-25 09:32:24 +0300

    Implement "redux-localstorage"

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,17 +3,28 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import currencySaga from '../sagas/Currency';
 import initialState from './initialState';
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import filter from 'redux-localstorage-filter';
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
+  const reducer = compose(
+    mergePersistedState()
+  )(rootReducer);
+  const storage = compose(
+    filter('transactions')
+  )(adapter(window.localStorage));
+  const enhancer = compose(
+    applyMiddleware(sagaMiddleware),
+    persistState(storage, 'redux-localstorage'),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return {
     ...createStore(
-      rootReducer,
+      reducer,
       initialState,
-      compose(
-        applyMiddleware(sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
+      enhancer
     ),
     runSaga: sagaMiddleware.run(currencySaga)
   };
